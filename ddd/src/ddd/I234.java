b8f813aaba3d17ba4e4fa06644f9657ec87f4027
chinbean <eiqqz@163.com>
2018-09-13 11:25:01 +0800

    ce

---
+++ ddd/src/ddd/I234.java
@@ -0,0 +1,24 @@
+package ddd;
+
+public class Dddd {
+private String name;
+private String age;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAge() {
+	return age;
+}
+public void setAge(String age) {
+	this.age = age;
+}
+@Override
+public String toString() {
+	return "Dddd [name=" + name + ", age=" + age + "]";
+}
+
+
+}
